e0489beee791373fd3d59fe1315230f90f963712
IsraelIyke <nwangwuisrael@gmail.com>
2023-11-17 16:18:06 +0100

    ok

--- telectify/src/app/login/page.tsx
+++ telectify/src/app/login/page.tsx
@@ -1,82 +1,61 @@
-"use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation"; // Import the Next.js router
-import supabase from "../client";
-import Head from "next/head";
-import Nav from "../components/nav";
+import React from "react";
+import Link from "next/link";
 
-export default function Auth() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const router = useRouter(); // Initialize the router
-
-  const handleSignIn = async () => {
-    try {
-      const { user, error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
-      if (error) {
-        alert(error.message);
-      } else {
-        alert("Signed in successfully");
-        // Redirect the user to the dashboard after successful sign-in
-        router.push("/dashboard");
-      }
-    } catch (error) {
-      alert(error.message);
-    }
-  };
-
+function Login() {
   return (
     <div className="min-h-screen bg-gray-300 border">
-      <Nav />
       <section className="w-4/5 lg:w-3/5 mx-auto">
-        <h3 className="mb-3 text-2xl font-bold text-left mt-8">
-          Login to your account
-        </h3>
-        <span className="mb-4 block">Enter your details correctly</span>
-        <div className="form-body">
-          <div className="form-field mb-8">
-            <label
-              htmlFor="address"
-              className="block mb-2 text-sm text-gray-900 font-bold"
-            >
-              House Address
-            </label>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-white-900 rounded-lg w-full p-2.5 focus:outline-none focus:ring-blue-500 focus:border-blue-500 "
-            />
+        <form className="form-container">
+          <h3 className="mb-3 text-2xl font-bold text-left mt-8">
+            Login to your account
+          </h3>
+          <span className="mb-4 block">Enter your details correctly</span>
+          <div className="form-body">
+            <div className="form-field mb-8">
+              <label
+                htmlFor="address"
+                className="block mb-2 text-sm text-gray-900 font-bold"
+              >
+                House Address
+              </label>
+              <input
+                type="text"
+                placeholder="N0 1 ziks drive"
+                id="address"
+                className="bg-gray-50 border border-gray-300 text-white-900 rounded-lg w-full p-2.5 focus:outline-none focus:ring-blue-500 focus:border-blue-500 "
+              />
+            </div>
+            <div className="form-field mb-8">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm text-gray-900 font-bold"
+              >
+                House ID
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your unique id"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-white-900 rounded-lg w-full p-2.5 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
+            <div className="form-field">
+              <button className="text-center bg-blue-600 rounded-lg text-white w-full block pb-2 pt-2">
+                Login
+              </button>
+            </div>
+            <p className="text-center text-sm font-semibold mt-4">
+              Do not have an account{" "}
+              <Link className="text-blue-600" href="/signup">
+                {" "}
+                Signup
+              </Link>{" "}
+            </p>
           </div>
-          <div className="form-field mb-8">
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm text-gray-900 font-bold"
-            >
-              House ID
-            </label>
-
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-white-900 rounded-lg w-full p-2.5 focus:outline-none focus:ring-blue-500 focus:border-blue-500 "
-            />
-          </div>
-
-          <button
-            className="text-center bg-blue-600 rounded-lg text-white w-full block pb-2 pt-2"
-            onClick={handleSignIn}
-          >
-            Sign In
-          </button>
-        </div>
+        </form>
       </section>
     </div>
   );
 }
+
+export default Login;
